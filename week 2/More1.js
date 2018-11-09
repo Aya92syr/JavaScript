07de3bd7727a4f5b5832a1fd6036006028f942a7
Aya Alhamwi <42946196+Aya92syr@users.noreply.github.com>
2018-11-10 00:39:51 +0100

    Add files via upload

---
+++ week 2/More1.js
@@ -0,0 +1,7 @@
+//sum of the an arguments.
+let x = myFunction(3, 5, 9);   
+
+function myFunction(a, b , c) {
+    return a + b + c ;            
+}
+console.log (x);
---
+++ week 2/more2.js
@@ -0,0 +1,8 @@
+
+//'a red car'
+
+function colorCar(color) 
+{
+    console.log("A\t"+ color + "\tcar\t !");
+}
+colorCar("Red");
