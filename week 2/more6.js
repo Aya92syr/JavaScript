439a40d05c24e1fcc4ae3d67b013a27fb84bd82a
Aya Alhamwi <42946196+Aya92syr@users.noreply.github.com>
2018-11-10 21:48:17 +0100

    Add files via upload

---
+++ week 2/more6.js
@@ -0,0 +1,10 @@
+function vehicleType ( color , object, age ) {
+    if (object == 1 && age == 5) {
+        console.log ("a " + color + " used " + "car" );
+    }
+    else {
+        console.log ("a " + color + "new" + " car");
+
+    }
+}
+vehicleType("blue", 1, 5)
