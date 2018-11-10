d2fce8304cf90b17f5b83edd9cfcfde2f070d7e7
Aya Alhamwi <42946196+Aya92syr@users.noreply.github.com>
2018-11-10 01:13:51 +0100

    Add files via upload

---
+++ week 2/more4.js
@@ -0,0 +1,10 @@
+function vehicleType ( color , object) {
+    if (object == 2) {
+        console.log ("a " + color + " motobike" );
+    }
+    else {
+        console.log ("a " + color + " car");
+
+    }
+}
+vehicleType("blue", 2)
