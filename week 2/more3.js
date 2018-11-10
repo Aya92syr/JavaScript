d4da6a69a2ee0500d3f0f15a7e637f7873ab83a9
Aya Alhamwi <42946196+Aya92syr@users.noreply.github.com>
2018-11-10 21:00:32 +0100

    Add files via upload

---
+++ week 2/more3.js
@@ -0,0 +1,10 @@
+let aboutMe = 
+{
+    name : "aya" ,
+    age : '26' ,
+    from : 'syria',
+};
+function tellMe ( name, age, from) {
+    console.log ('my name is :' + aboutMe.name + ' ' + '\nI \' m : ' + aboutMe.age +  ' ' + '\nI\'m from :' + aboutMe.from);
+}
+tellMe ();
